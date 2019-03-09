aab650b76f05072f579a295defd9488ec8270e5a
505856727 <30371192+505856727@users.noreply.github.com>
2019-03-09 22:34:40 +0800

    资源导入，游戏控制单例

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : DDOLSingleton<GameManager>
+{
+    public void Initialization()
+    {
+        gameObject.AddComponent<AudioSource>();
+    }
+    //播放音乐一系列重载
+    #region
+    public void PlayMusic(int number)
+    {
+        
+    }
+
+    public void PlayMusic(int number,int volume)
+    {
+
+    }
+
+    public void PlayMusic(int number,int volume,int pitch)
+    {
+
+    }
+
+    public void PlayMusic(int number, int volume, int pitch, bool loop)
+    {
+
+    }    
+
+    public void PlayMusic(int number,bool loop)
+    {
+
+    }
+    #endregion
+
+
+}
